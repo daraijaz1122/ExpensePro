db61738afa81566e861fa873c9655e2bfc212c91
aijaz <saijazahmad38@gmail.com>
2025-07-13 16:54:01 +0530

    added .env

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "drizzle-kit";
-// import * as dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
-// dotenv.config({ path: ".env" });
+dotenv.config({ path: ".env" });
 
 export default defineConfig({
   schema: "./utils/schema.js",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_cMl0WKRzNZ9Y@ep-plain-queen-a1i85jwl-pooler.ap-southeast-1.aws.neon.tech/neondb",
+    url: process.env.DATABASE_URL 
   },
 });
