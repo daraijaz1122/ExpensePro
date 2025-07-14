0f2da66f2405f7bbfa6be0040c18984618986cb1
aijaz <saijazahmad38@gmail.com>
2025-07-14 19:47:47 +0530

    updated readme

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,34 +1,33 @@
-"use client"
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import React from 'react';
-import {useUser,UserButton} from '@clerk/nextjs';
-import { useRouter } from 'next/navigation';
-
-
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+import { useUser, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
   const Push = useRouter().push;
-const {user,isSignedIn} = useUser();
+  const { user, isSignedIn } = useUser();
   return (
-      <div className='p-5 flex items-center justify-between border shadow-md'>
-          <Image src={"/logo.png"}
-              alt='logo'
-              width={150}
-              height={90}
-      />
-      {
-        isSignedIn ?
-        <UserButton /> :
-      
-          <Button
-       onClick={(()=>Push('/sign-in'))}
-            className='bg-indigo-600 px-8 py-6 text-l text-white'>
-        Get Started
-          </Button>
-      }
+    <div className="p-5 flex items-center justify-between border shadow-md">
+      <Image src={"/logo.png"} alt="logo" width={150} height={90} />
+      {isSignedIn ? (
+        <Button
+          className="bg-indigo-600 px-8 py-6 text-lg text-white"
+          onClick={() => Push("/dashboard")}
+        >
+          Dashboard
+        </Button>
+      ) : (
+        <Button
+          onClick={() => Push("/sign-in")}
+          className="bg-indigo-600 px-8 py-6 text-lg text-white"
+        >
+          Get Started
+        </Button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
